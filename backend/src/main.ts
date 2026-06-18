5f0b9f4d32515eb95482efb2e5d1b2116a1d1622
Julian-Ayuso <jnayuso@hotmail.com>
2026-06-17 21:05:17 -0300

    3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+  origin: [
+    'https://tp22-77653.web.app',
+    'https://tp22-77653.firebaseapp.com',
+    'http://localhost:4200',
+  ],
+  credentials: true,
+});
+  //app.enableCors({ origin: true, credentials: true });
   //app.enableCors({
   //  origin: ['https://tp22-77653.web.app', 'https://tp22-77653.firebaseapp.com', 'http://localhost:4200'],
   //  credentials: true,
